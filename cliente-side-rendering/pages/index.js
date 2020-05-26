e737ade4546306b97fb6adc18924b3ba74fd1783
Rodrigo Gama <ro.gama91@gmail.com>
2020-05-25 22:59:45 -0300

    feat: adding client-side rendering

--- cliente-side-rendering/pages/index.js
+++ cliente-side-rendering/pages/index.js
@@ -1,6 +1,35 @@
+import React from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
+
+const usePokemon = (query) => {
+  const [pokemons, setPokemons] = React.useState(null);
+
+  React.useEffect(() => {
+    if (!query) {
+      setPokemons(null);
+      return;
+    }
+
+    const getPokemon = async () => {
+      const data = await fetch(`/api/search?q=${escape(query)}`).then(res => res.json());
+      setPokemons(data.map(e => ({
+        ...e,
+        image: `/images/${e.id}.png`
+      })));
+    }
+
+    getPokemon();
+  }, [query]);
+
+  return pokemons;
+}
+
 
 export default function Home() {
+  const [query, setQuery] = React.useState('');
+  const pokemons = usePokemon(query);
+
   return (
     <div className="container">
       <Head>
@@ -9,7 +38,19 @@ export default function Home() {
       </Head>
 
       <main>
-        <p>Hello!</p>
+        <input placeholder="Search for a pokemon" type="search" value={query} onChange={event => setQuery(event.target.value)} />
+
+        <div className="row">
+          {pokemons && pokemons.map(pokemon => (
+            <Link key={pokemon.id} href={`pokemon/${pokemon.name.toLowerCase()}`}>
+              <div className="card clickable">
+                <h3 className="card--title">{pokemon.name}</h3>
+                <span className="card--subtitle">{pokemon.type.join(', ')}</span>
+                <img src={pokemon.image} alt={pokemon.name} />
+              </div>
+            </Link>
+          ))}
+        </div>
       </main>
     </div>
   )
