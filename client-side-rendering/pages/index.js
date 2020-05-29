1e33775c9bdda7c49cfedb59b5a59991505af870
Rodrigo Gama <ro.gama91@gmail.com>
2020-05-29 17:57:21 -0300

    starter mock

--- client-side-rendering/pages/index.js
+++ client-side-rendering/pages/index.js
@@ -1,16 +1,44 @@
-import Head from 'next/head'
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
+  const [query, setQuery] = React.useState("");
+  const pokemons = [];
+
   return (
     <div className="container">
       <Head>
-        <title>Pokemon</title>
+        <title>Pokemon - Client Side</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <p>Hello!</p>
+        <input
+          placeholder="Search for a pokemon"
+          type="search"
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+        />
+
+        <div className="row">
+          {pokemons &&
+            pokemons.map((pokemon) => (
+              <Link
+                key={pokemon.id}
+                href={`pokemon/${pokemon.name.toLowerCase()}`}
+              >
+                <div className="card clickable">
+                  <h3 className="card--title">{pokemon.name}</h3>
+                  <span className="card--subtitle">
+                    {pokemon.type.join(", ")}
+                  </span>
+                  <img src={pokemon.image} alt={pokemon.name} />
+                </div>
+              </Link>
+            ))}
+        </div>
       </main>
     </div>
-  )
+  );
 }
